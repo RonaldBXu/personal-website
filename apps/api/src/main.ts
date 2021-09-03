c9d0f2d2d1069e57dfca0671692ab8142190836c
RonaldBXu <ronald@cresicor.com>
2021-09-03 13:26:26 -0400

    api

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -11,8 +11,7 @@ app.get('/api', (req, res) => {
   res.send({ message: 'Welcome to api!' });
 });
 
-const port = process.env.port || 3333;
-const server = app.listen(port, () => {
-  console.log(`Listening at http://localhost:${port}/api`);
+const server = app.listen(process.env.port || 3333, () => {
+  console.log(`Listening...`);
 });
 server.on('error', console.error);
